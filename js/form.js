63be9fa4af39c6137d064022a0f3e642a161a974
Alexandra <acederys@gmail.com>
2020-02-29 12:52:29 +0300

    исправлены ошибки

--- js/form.js
+++ js/form.js
@@ -1,20 +1,13 @@
+// eslint-disable-next-line strict
 'use srtict';
-// В случае выбора количества комнат, менялся список опций у гостей
-// потребуется такой хитрый объект
-// он делает соответствие между кол-вом гостей и кол-вом комнат для них
 
+(function () {
 
-
-// нажатие на элемент .map__pin-main приводит к вводу страницы в активное состояние
-// .map перестает содержать .map--faded
-// .ad-form перестает содержать .ad-form--disabled
-// у input, select, .ad-form или из родительского элемента fieldset убирается disabled !!! removeAttribute('disabled');
-// у .map__filters убирается disabled
-
-(function() {
   var addressInput = document.querySelector('#address');
   console.log(addressInput);
 
+  var mapActive = document.querySelector('.map__pin--main');
+  console.log(mapActive);
 
   var type = document.querySelector('#type');
   console.log(type);
@@ -58,116 +51,127 @@
   };
 
   var adForm = document.querySelector('.ad-form');
-console.log(adForm);
-// переменная содержит ваше обьявление
+  console.log(adForm);
+  // переменная содержит ваше обьявление
 
-var inputForm = adForm.querySelectorAll('input');
-console.log(inputForm);
+  var inputForm = adForm.querySelectorAll('input');
+  console.log(inputForm);
 
-// переменная содержит input находящиеся внутри формы обьявления
+  // переменная содержит input находящиеся внутри формы обьявления
 
-var selectForm = adForm.querySelectorAll('select');
-console.log(selectForm);
+  var selectForm = adForm.querySelectorAll('select');
+  console.log(selectForm);
 
-// переменная содержит select внутри формы обьявления
+  // переменная содержит select внутри формы обьявления
 
-var coordY = window.map.mapActive.offsetTop + window.map.PIN_HEIGHT;
-console.log(coordY);
+  var PIN_WIDTH = mapActive.offsetWidth;
+console.log(PIN_WIDTH);
 
-// в переменную положили кординату острого угла метки по высоте
+//ширина метки
 
-var coordX = window.map.mapActive.offsetLeft + (0.5 * window.map.PIN_WIDTH);
-console.log(coordX);
+var PIN_HEIGHT = mapActive.offsetHeight;
+console.log(PIN_HEIGHT);
 
-var addAdress = function () {
-  var valueAddress = document.querySelector('#address').value = coordX + ',' + coordY;
-  console.log(valueAddress);
-  return valueAddress;
-};
+//высота метки
+
+  var coordY = mapActive.offsetTop + PIN_HEIGHT;
+  console.log(coordY);
+
+  // в переменную положили кординату острого угла метки по высоте
+
+  var coordX = mapActive.offsetLeft + (0.5 * PIN_WIDTH);
+  console.log(coordX);
+
+  var address = adForm.querySelector('#address');
+
+  var addAddress = function(coordX, coordY) {
+    address.value = coordX + ', ' + coordY;
+  }
 
   var calculateGuestsNumber = function (value) {
     capacityList.forEach(function (option) {
-    option.disabled = true;
+      option.disabled = true;
+    });
+
+    roomOptions[value].forEach(function (it) {
+      capacityList.forEach(function (opt) {
+        if (Number(opt.value) === it) {
+          opt.disabled = false;
+          opt.selected = true;
+        }
+      });
+    });
+  }
+
+  calculateGuestsNumber(roomNumber.value);
+
+  roomNumber.addEventListener('change', function (event) {
+    calculateGuestsNumber(event.target.value);
+  })
+
+  type.addEventListener('change', function (evt) {
+    // при убирании таргета с типа жилья он должен показывать стоимость в placeholder
+    switch (evt.target.value) {
+      case 'bungalo':
+        price.min = 0;
+        price.placeholder = '0';
+        break;
+      case 'flat':
+        price.min = 1000;
+        price.placeholder = '1000';
+        break;
+      case 'house':
+        price.min = 5000;
+        price.placeholder = '5000';
+        break;
+      case 'palace':
+        price.min = 10000;
+        price.placeholder = 10000;
+        break;
+    }
   });
 
-  roomOptions[value].forEach(function (it) {
-    capacityList.forEach(function (opt) {
-      if (Number(opt.value) === it) {
-        opt.disabled = false;
-        opt.selected = true;
-      }
+  timeIn.addEventListener('change', function (evt) {
+    // при задании времени вьезда задается время вызда
+    timeOut.value = evt.target.value;
   });
-});}
 
-calculateGuestsNumber(roomNumber.value);
+  timeOut.addEventListener('change', function (evt) {
+    // при задании времени задается вызда время вьезда
+    timeIn.value = evt.target.value;
+  });
 
-roomNumber.addEventListener('change', function (event) {
-  calculateGuestsNumber(event.target.value);
-})
+  capacity.addEventListener('change', function (evt) {
+    evt.target.setCustomValidity('');
+  });
 
-type.addEventListener('change', function (evt) {
-  // при убирании таргета с типа жилья он должен показывать стоимость в placeholder
-  switch (evt.target.value) {
-    case 'bungalo':
-      price.min = 0;
-      price.placeholder = '0';
-      break;
-    case 'flat':
-      price.min = 1000;
-      price.placeholder = '1000';
-      break;
-    case 'house':
-      price.min = 5000;
-      price.placeholder = '5000';
-      break;
-    case 'palace':
-      price.min = 10000;
-      price.placeholder = 10000;
-      break;
-  }
-});
+  var disableForm = function () {
+    for (var i = 0; i < inputForm.length; i++) {
+      inputForm[i].removeAttribute('disabled', 'true')
+    }
 
-timeIn.addEventListener('change', function (evt) {
-  // при задании времени вьезда задается время вызда
-  timeOut.value = evt.target.value;
-});
-
-timeOut.addEventListener('change', function (evt) {
-  // при задании времени задается вызда время вьезда
-  timeIn.value = evt.target.value;
-});
-
-capacity.addEventListener('change', function (evt) {
-  evt.target.setCustomValidity('');
-});
-
-var disableForm = function() {
-  for (var i = 0; i < form.inputForm.length; i++) {
-    form.inputForm[i].removeAttribute('disabled', 'true')
-   }
-
-  for (var i = 0; i < form.selectForm.length; i++) {
-    form.selectForm[i].removeAttribute('disabled', 'true')
-  }
-};
-
-var enableForm = function () {
-  adForm.classList.remove('ad-form--disabled');
-
-  for (var i = 0; i < form.inputForm.length; i++) {
-    form.inputForm[i].removeAttribute('disabled', 'false');
+    for (var i = 0; i < selectForm.length; i++) {
+      selectForm[i].removeAttribute('disabled', 'true')
+    }
   };
 
-  for (var i = 0; i < form.selectForm.length; i++) {
-    form.selectForm[i].removeAttribute('disabled', 'false');
+  var enableForm = function () {
+    adForm.classList.remove('ad-form--disabled');
+
+    for (var i = 0; i < inputForm.length; i++) {
+      inputForm[i].removeAttribute('disabled', 'false');
+    };
+
+    for (var i = 0; i < selectForm.length; i++) {
+      selectForm[i].removeAttribute('disabled', 'false');
+    };
   };
-};
 
-// экспортируем две функции, чтобы использовать их в map.js
-window.form = {
-  disableForm: disableForm,
-  enableForm: enableForm,
-  addAdress: addAdress
-};
+  // экспортируем две функции, чтобы использовать их в map.js
+  window.form = {
+    disableForm: disableForm,
+    enableForm: enableForm,
+    addAddress: addAddress
+  };
 
-}) ();
+})();
--- js/map.js
+++ js/map.js
@@ -1,14 +1,8 @@
 'use srtict';
 
-// нажатие на элемент .map__pin-main приводит к вводу страницы в активное состояние
-// .map перестает содержать .map--faded
-// .ad-form перестает содержать .ad-form--disabled
-// у input, select, .ad-form или из родительского элемента fieldset убирается disabled !!! removeAttribute('disabled');
-// у .map__filters убирается disabled
-
 (function() {
 
-  window.form.disableForm();
+window.form.disableForm();
 
   var mapActive = document.querySelector('.map__pin--main');
 console.log(mapActive);
@@ -24,44 +18,25 @@ var mapFilter = document.querySelector('.map__filters');
 console.log(mapFilter);
 // переменная содержит фильтр обьявлений
 
-var PIN_WIDTH = window.map.mapActive.offsetWidth;
-console.log(PIN_WIDTH);
-
-//ширина метки
-
-var PIN_HEIGHT = window.map.mapActive.offsetHeight;
-console.log(PIN_HEIGHT);
-
-//высота метки
-
 var openMap = function () {
   map.classList.remove('map--faded');
   // у карты убирается класс
 };
 
-mapActive.addEventListener('mousedown', function
-  // при клике активируется функция
-  () {
+mapActive.addEventListener('mousedown',
+ function() {
     openMap();
-    openAdForm();
     window.form.enableForm();
-    window.form.addAddress(coordX, coordY);
+    window.form.addAddress();
   });
 
 mapActive.addEventListener('keydown',
   function (evt) {
     if (evt.key === ENTER_KEY) {
       openMap();
-      openAdForm();
       window.form.enableForm();
-      window.form.addAddress(coordX, coordY);
-    }
+      window.form.addAddress();
+    };
   });
-
-  window.map = {
-    mapActive: mapActive,
-    PIN_WIDTH: PIN_WIDTH,
-    PIN_HEIGHT: PIN_HEIGHT
-  }
 
 })();
